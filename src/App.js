05bfb048b7e7a0e6185e553c18558afb2a9091db
enwadiogbu <emekanwadiogbu.jr@me.com>
2023-05-15 19:11:54 -0700

    building the counter app plus and minus buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
+
+function counter(){
+  alert('You clicked Me!');
+}
+
 function App() {
+  const myAge = 20;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h2 style={{color: 'red'}}>Hello, I am {myAge}</h2>
+      <button onClick={() => alert('You clicked the + button')}>+</button>
+      <button onClick={() => alert('You clicked the - button')}>-</button>
+    </>
+
+    
+
+  )
 }
 
 export default App;
